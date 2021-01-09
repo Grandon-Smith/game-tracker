f9540203a46bdacac8f4d5a16f1f059e3e7bfbb2
Grandon Smith <grandon.smith@yahoo.com>
2021-01-09 12:33:45 -0500

    initial commit

---
+++ src/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './nav.css'
+
+export default function Nav() {
+    return (
+        <header>
+            <h1><a href="#">Game Saver</a></h1>
+            <nav>
+                <input type="button" value="log in"/>
+            </nav>
+        </header>
+    )
+}
