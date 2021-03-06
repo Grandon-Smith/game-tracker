462d1b2faa15cd47e010861b0ee6ba05415b8d22
Grandon Smith <grandon.smith@yahoo.com>
2021-03-05 22:40:27 -0500

    styling login

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -16,29 +16,39 @@ export default class LoginForm extends React.Component {
             <div className="login-container">
                 <form onSubmit={e => this.props.login(e, this.state.loginUsername, this.state.loginPassword)}>
                     <fieldset className="login-form">
-                        <legend>Login:</legend>
+                        <legend>Login</legend>
                         <div className="form-section">
-                            <label htmlFor="username"></label>
+                            <label 
+                                htmlFor="username" 
+                                className="visually-hidden">
+                                    Email
+                            </label>
                             <input 
                                 id="username" 
                                 type="text"
                                 onChange={e => this.setState({loginUsername: e.target.value})}
-                                placeholder="Email:"
+                                placeholder=" Email"
+                                required
                             />
                         </div>
                         <div className="form-section">
-                            <label htmlFor="password">Password: </label>
+                            <label 
+                                htmlFor="password" 
+                                className="visually-hidden">
+                                    Password
+                            </label>
                             <input 
                                 id="password" 
                                 type="password" 
                                 name="password" 
+                                placeholder=" Password"
                                 onChange={e => this.setState({loginPassword: e.target.value})}
                                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
                                 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" 
                                 required
                             />
                         </div>
-                        <div className="login-form-buttons">
+                        <div className="login-form-buttons form-section">
                             <button type="submit">Log In</button>
 
                             <p> Don't have an account?</p>
